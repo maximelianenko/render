bb04a08009d46599313e10703c32adc351c66610
m e <maximelianenko@gmail.com>
2024-08-19 02:59:46 +0300

    refactor and better responses

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,135 @@ use actix_multipart::{form::MultipartFormConfig, MultipartError};
 
 use actix_web::{error::Error, App, HttpRequest, HttpServer};
 
+use http_serde::http::StatusCode;
 // use tokio::join;
-use renderenko::post::render::render_config;
-// use http_serde::http::StatusCode as SerdeStatusCode;
+use renderenko::post::render::{render_config, APIResponse};
+
 
 fn error_handler(err:MultipartError, _req: &HttpRequest) -> Error {
-    Error::from(err)
+    // println!("Error: {:?}\n Request: {:?}",err,req);
+    let res = match err {
+        MultipartError::NoContentDisposition => {
+            APIResponse::new(
+                StatusCode::BAD_REQUEST,
+                "Content-Disposition",
+                "wrong Content-Disposition header is set",
+                ""
+            )
+        },
+        MultipartError::NoContentType => {
+            APIResponse::new(
+                StatusCode::UNSUPPORTED_MEDIA_TYPE,
+                "Content-Type",
+                "wrong Content-Type header is set",
+                ""
+            )
+        },
+        MultipartError::ParseContentType => {
+            APIResponse::new(
+                StatusCode::BAD_REQUEST,
+                "Content-Type",
+                "couldn't parse Content-Type header",
+                ""
+            )
+        },
+        MultipartError::Boundary => {
+            APIResponse::new(
+                StatusCode::BAD_REQUEST,
+                "multipart",
+                "multipart boundary is not found",
+                ""
+            )
+        },
+        MultipartError::Nested => {
+            APIResponse::new(
+                StatusCode::BAD_REQUEST,
+                "multipart",
+                "nested multipart is not supported",
+                ""
+            )
+        },
+        MultipartError::Incomplete => {
+            APIResponse::new(
+                StatusCode::BAD_REQUEST,
+                "multipart",
+                "multipart stream is incomplete",
+                ""
+            )
+        },
+        MultipartError::Parse(_err) => {
+            APIResponse::new(
+                StatusCode::BAD_REQUEST,
+                "",
+                "error during field parsing",
+                ""
+            )
+        },
+        MultipartError::Payload(_err) => {
+            APIResponse::new(
+                StatusCode::BAD_REQUEST,
+                "",
+                "payload error",
+                ""
+            )
+        },
+        MultipartError::NotConsumed => {
+            APIResponse::new(
+                StatusCode::BAD_REQUEST,
+                "",
+                "not consumed",
+                ""
+            )
+        },
+        MultipartError::Field { field_name, source } => {
+            APIResponse::new(
+                StatusCode::BAD_REQUEST,
+                &field_name,
+                &format!("deserialize error in field '{}'", field_name),
+                &source.to_string()
+            )
+        },
+        MultipartError::DuplicateField(field_name) => {
+            APIResponse::new(
+                StatusCode::BAD_REQUEST,
+                &field_name,
+                &format!("dublicate field {}", field_name),
+                ""
+            )
+        },
+        MultipartError::MissingField(field_name) => {
+            APIResponse::new(
+                StatusCode::BAD_REQUEST,
+                &field_name,
+                &format!("missing required field '{}'", field_name),
+                ""
+            )
+        },
+        MultipartError::UnsupportedField(field_name) => {
+            APIResponse::new(
+                StatusCode::BAD_REQUEST,
+                &field_name,
+                &format!("unsupported field {}", field_name),
+                ""
+            )
+        },
+        _ => {
+            APIResponse::new(
+                StatusCode::IM_A_TEAPOT,
+                "unknown",
+                "unknown error",
+                "idk"
+            )
+        },
+    };
+    
+    return Error::from(res)
+    // Error::from(APIResponse {
+    //     field: err.field_name,
+    //     message: String::from("field"),
+    //     description: String::from("field")
+    // })
+    // Error::from(RenderenkoError {name: "something"})
 }
 
 #[actix_web::main] 
--- src/post/render.rs
+++ src/post/render.rs
@@ -1,12 +1,13 @@
+use core::fmt;
 use std::{borrow::Borrow, fs::File, io::{BufReader, BufWriter}, path::Path, sync::Arc};
 
 use actix_multipart::form::{tempfile::TempFile, text::Text, MultipartForm};
-use actix_web::{http::StatusCode, web, HttpResponse, Responder};
+use actix_web::{http::StatusCode as ActixStatusCode, web, HttpResponse, Responder, ResponseError};
 
-use axum::http::header;
+// use axum::{http::header, response::AppendHeaders};
 use image::{codecs::png::PngEncoder, ExtendedColorType, ImageEncoder, ImageFormat};
 use serde::Serialize;
-use http_serde::http::StatusCode as SerdeStatusCode;
+use http_serde::http::StatusCode;
 use crate::rend::{builder, misc::mesh::load_from_obj, types::MeshV4Plus};
 pub struct AppState {
     // ferris: Renderenko,
@@ -22,76 +23,31 @@ pub enum Skin {
 #[derive(Debug, MultipartForm)]
 pub struct UploadForm {
     #[multipart(limit = "1MB")]
-    skin_texture: TempFile,
-    width: Option<Text<u32>>,
-    height: Option<Text<u32>>,
-    resize: Option<Text<bool>>,
-    resize_width: Option<Text<u32>>,
-    resize_height: Option<Text<u32>>,
-    skin_type: Option<Text<String>>,
-    fov: Option<Text<f32>>,
-    far: Option<Text<f32>>,
-    near: Option<Text<f32>>,
+    texture: TempFile,
+    width: Text<u32>,
+    height: Text<u32>,
+    resize: Text<bool>,
+    resize_width: Text<u32>,
+    resize_height: Text<u32>,
+    skin_type: Text<String>,
+    fov: Text<f32>,
+    far: Text<f32>,
+    near: Text<f32>,
 
-    cam_pos_x: Option<Text<f32>>,
-    cam_pos_y: Option<Text<f32>>,
-    cam_pos_z: Option<Text<f32>>,
+    cam_pos_x: Text<f32>,
+    cam_pos_y: Text<f32>,
+    cam_pos_z: Text<f32>,
 
-    cam_rot_x: Option<Text<f32>>,
-    cam_rot_y: Option<Text<f32>>,
+    cam_rot_x: Text<f32>,
+    cam_rot_y: Text<f32>,
 
-    mod_pos_x: Option<Text<f32>>,
-    mod_pos_y: Option<Text<f32>>,
-    mod_pos_z: Option<Text<f32>>,
+    mod_pos_x: Text<f32>,
+    mod_pos_y: Text<f32>,
+    mod_pos_z: Text<f32>,
     
-    mod_rot_x: Option<Text<f32>>,
-    mod_rot_y: Option<Text<f32>>,
+    mod_rot_x: Text<f32>,
+    mod_rot_y: Text<f32>,
 
-}
-
-macro_rules! text_unwrap_or {
-    ( $x:expr, $y:expr ) => {
-        {
-            let field;
-            if $x.is_some() {
-                field = $x.unwrap().into_inner()
-            } else {
-                field = $y
-            };
-            field
-        }
-    };
-}
-macro_rules! minmax {
-    ( $min: expr, $max: expr, $value:expr) => {
-            {
-                let field;
-                if $value > $min {
-                    if $value < $max {
-                        field = $value;
-                    } else {
-                        field = $max;
-                    }
-                } else {
-                    field = $min;
-                }
-                field
-            }
-    }
-}
-macro_rules! minmax_text_unwrap_or {
-    ( $min: expr, $max: expr, $x:expr, $y:expr) => {
-        {
-            let field;
-            if $x.is_some() {
-                let value = $x.unwrap().into_inner();
-                field = minmax!($min,$max,value);
-            } else {
-                field = $y;
-            };
-            field
-        }
-    };
 }
 // fn config(cfg: &mut web::ServiceConfig) {
 //     cfg.service(
@@ -115,42 +71,113 @@ pub fn render_config(cfg: &mut web::ServiceConfig) {
                 .route(web::post().to(render))
         );
 }
-#[derive(Serialize, Debug)]
+
+// macro_rules! block_or_response {
+//     ($field: expr,$value:expr, $block:block) => {
+//         if ($value.is_some()) {
+//             $block
+//         } else {
+//             return APIResponse::new(
+//                 StatusCode::BAD_REQUEST,
+//                 $field,
+//                 &format!("missing '{}' field", $field),
+//                 ""
+//             ).response_json()
+//         }
+//     };
+// }
+
+// '`"временное решение"`'
+macro_rules! var_minmax_or_response {
+    ($field:ident, $value:expr, $min:expr, $max:expr) => {
+        let $field;
+        minmax_or_response!($field,$value,$min,$max);
+    };
+}
+macro_rules! minmax_or_response {
+    ($field:ident, $value:expr, $min:expr, $max:expr) => {
+        let value = $value.into_inner();
+        if value >= $min && value <= $max {
+            $field = value
+        } else {
+            return APIResponse::new(
+                StatusCode::BAD_REQUEST,
+                stringify!($field),
+                &format!("field '{}' is out of limits", stringify!($field)),
+                &format!("field '{}' value needs to be in range {}-{}",stringify!($field),$min,$max)
+            ).response_json()
+        }
+    };
+}
+#[derive(Serialize, Debug, Clone)]
 pub struct APIResponse {
     #[serde(with = "http_serde::status_code")]
-    pub code: SerdeStatusCode,
+    pub code: StatusCode,
+    pub field: String,
     pub message: String,
     pub description: String
 }
 
+impl APIResponse {
+    pub fn new(code: StatusCode, field: &str, message: &str, description: &str) -> APIResponse {
+        APIResponse {
+            code,
+            field: field.to_string(),
+            message: message.to_string(),
+            description: description.to_string()
+        }
+    }
+    pub fn response_json(&self) -> HttpResponse {
+        HttpResponse::build(self.status_code())
+            .append_header(("Access-Control-Allow-Origin","*"))
+            .append_header(("Access-Control-Allow-Methods", "POST"))
+            .json(&self)
+    }
+}
+
+impl fmt::Display for APIResponse {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Oh no, something bad went down")
+    }
+}
+impl ResponseError for APIResponse {
+    fn status_code(&self) -> ActixStatusCode {
+        ActixStatusCode::from_u16(self.code.into()).unwrap_or(ActixStatusCode::INTERNAL_SERVER_ERROR)
+    }
+
+    fn error_response(&self) -> HttpResponse<actix_web::body::BoxBody> {
+        return self.response_json()
+    }
+}
 pub async fn render(MultipartForm(form): MultipartForm<UploadForm>,data: web::Data<AppState>) -> impl Responder {
-    if form.skin_texture.content_type.is_none() || form.skin_texture.content_type.unwrap() != "image/png" {
-        return HttpResponse::build(StatusCode::BAD_REQUEST).json(APIResponse {
-            code: SerdeStatusCode::BAD_REQUEST,
-            message: String::from("texture must be in png format"),
-            description: String::from("texture must be an image in png format and have size up to ~1MB")
-        })
-        // return HttpResponse::build(StatusCode::BAD_REQUEST).body("Only png images are supported");
+    if form.texture.content_type.is_none() || form.texture.content_type.unwrap() != "image/png" {
+        return APIResponse::new(
+                StatusCode::BAD_REQUEST,
+            "texture",
+            "texture must be in png format",
+            "texture must be an image in png format and have size up to ~1MB"
+        ).response_json()
     }
     
-    let file = match File::open(form.skin_texture.file) {
+    let file = match File::open(form.texture.file) {
         Err(_error) => {
-            return HttpResponse::build(StatusCode::BAD_REQUEST).json(APIResponse {
-                code: SerdeStatusCode::BAD_REQUEST,
-                message: String::from("there's something wrong with the texture"),
-                description: String::from("texture must be in png format and up to ~1MB in size")
-            })
-            // return HttpResponse::build(StatusCode::BAD_REQUEST).body(format!("Something wrong with file. \n Error: {}", error));
+            return APIResponse::new(
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "texture",
+                "texture file couldn't be opened on the server",
+                "maybe texture file is corrupted"
+            ).response_json()
         }
         Ok(value) => value
     };
     let texture = match image::load(BufReader::new(file), ImageFormat::Png) {
         Err(_error) => {
-            return HttpResponse::build(StatusCode::BAD_REQUEST).json(APIResponse {
-                code: SerdeStatusCode::BAD_REQUEST,
-                message: String::from("there's something wrong with the texture"),
-                description: String::from("texture must be in png format and up to ~1MB in size")
-            })
+            return APIResponse::new(
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "texture",
+                "texture file couldn't be readed on the server",
+                "maybe texture file is corrupted"
+            ).response_json()
         }
         Ok(value) => value
     };
@@ -159,49 +186,76 @@ pub async fn render(MultipartForm(form): MultipartForm<UploadForm>,data: web::Da
     // println!("{}",texture_aspect_ratio);
 
     if texture_aspect_ratio != 1 && texture_aspect_ratio != 2 {
-        return HttpResponse::build(StatusCode::BAD_REQUEST).json(APIResponse {
-            code: SerdeStatusCode::BAD_REQUEST,
-            message: String::from("texture have wrong aspect ratio"),
-            description: String::from("Texture aspect-ratio needs to be 1/1 or 2/1 (64x64, 256x256, 64x32 etc...)")
-        })
+        return APIResponse::new(
+            StatusCode::BAD_REQUEST,
+            "texture",
+            "texture have wrong aspect ratio",
+            "texture aspect-ratio needs to be 1/1 or 2/1 (64x64, 256x256, 64x32 etc...)"
+        ).response_json()
     }
 
-    let width = minmax_text_unwrap_or!(10,1000,form.width,100);
-    let height = minmax_text_unwrap_or!(10,1000,form.height,100);
-    let resize = text_unwrap_or!(form.resize, false);
-    let resize_width = minmax_text_unwrap_or!(10,1000,form.resize_width,500);
-    let resize_height = minmax_text_unwrap_or!(10,1000,form.resize_height,500);
+    var_minmax_or_response!(width,form.width, 10, 1000);
+    var_minmax_or_response!(height,form.height, 10, 1000);
+    // let width = minmax_text_unwrap_or!(10,1000,form.width,100);
+    // let resize = text_unwrap_or!(form.resize, false);
+    let resize = form.resize.into_inner();
+    // minmax_or_response!(resize_width,form.resize_width, 10, 1000);
+    // minmax_or_response!(resize_height,form.resize_height, 10, 1000);
+    // let resize_width = minmax_text_unwrap_or!(10,1000,form.resize_width,500);
+    // let resize_height = minmax_text_unwrap_or!(10,1000,form.resize_height,500);
 
-    let skin_type: Skin;
-    if texture_aspect_ratio == 2 {
-        skin_type = Skin::SteveOld
-    } else {
-        let skin_type_string = text_unwrap_or!(form.skin_type,String::from("alex")).to_lowercase();
-        if &skin_type_string == "steve" {
-            skin_type = Skin::Steve
-        } else {
-            skin_type = Skin::Alex
-        }
-    }
+    let skin_type_string = form.skin_type.into_inner();
 
-    let fov = minmax_text_unwrap_or!(1.0, 120.0, form.fov, 30.0);
+    let skin_type: Skin = match skin_type_string.as_str() {
+            "steve" => {
+                Skin::Steve
+            }
+            "steve_old" => {
+                Skin::SteveOld
+            }
+            _ => {
+                Skin::Alex
+            }
+    };
+    // if texture_aspect_ratio == 2 {
+    //     skin_type = Skin::SteveOld
+    // } else {
+    //     let skin_type_string = text_unwrap_or!(form.skin_type,String::from("alex")).to_lowercase();
+    //     if &skin_type_string == "steve" {
+    //         skin_type = Skin::Steve
+    //     } else {
+    //         skin_type = Skin::Alex
+    //     }
+    // }
 
-    let far = minmax_text_unwrap_or!(1.0,1000.0,form.far, 1000.0);
-    let near = minmax_text_unwrap_or!(0.1,1000.0,form.near, 0.1);
+    // let fov = minmax_text_unwrap_or!(1.0, 120.0, form.fov, 30.0);
+    var_minmax_or_response!(fov,form.fov, 1.0, 360.0);
+    var_minmax_or_response!(far,form.far, 1.0, 1000.0);
+    var_minmax_or_response!(near,form.near, 0.1, 1000.0);
+    // let far = minmax_text_unwrap_or!(1.0,1000.0,form.far, 1000.0);
+    // let near = minmax_text_unwrap_or!(0.1,1000.0,form.near, 0.1);
+    var_minmax_or_response!(cam_pos_x,form.cam_pos_x, -1000.0, 1000.0);
+    var_minmax_or_response!(cam_pos_y,form.cam_pos_y, -1000.0, 1000.0);
+    var_minmax_or_response!(cam_pos_z,form.cam_pos_z, -1000.0, 1000.0);
+    // let cam_pos_x = minmax_text_unwrap_or!(-1000.0,1000.0,form.cam_pos_x,0.0);
+    // let cam_pos_y = minmax_text_unwrap_or!(-1000.0,1000.0,form.cam_pos_y,0.0);
+    // let cam_pos_z = minmax_text_unwrap_or!(-1000.0,1000.0,form.cam_pos_z,0.0);
+    var_minmax_or_response!(cam_rot_x,form.cam_rot_x, -360.0, 360.0);
+    var_minmax_or_response!(cam_rot_y,form.cam_rot_y, -360.0, 360.0);
+    // let cam_rot_x = minmax_text_unwrap_or!(-360.0,360.0,form.cam_rot_x,0.0);
+    // let cam_rot_y = minmax_text_unwrap_or!(-360.0,360.0,form.cam_rot_y,0.0);
 
-    let cam_pos_x = minmax_text_unwrap_or!(-1000.0,1000.0,form.cam_pos_x,0.0);
-    let cam_pos_y = minmax_text_unwrap_or!(-1000.0,1000.0,form.cam_pos_y,0.0);
-    let cam_pos_z = minmax_text_unwrap_or!(-1000.0,1000.0,form.cam_pos_z,0.0);
-
-    let cam_rot_x = minmax_text_unwrap_or!(-360.0,360.0,form.cam_rot_x,0.0);
-    let cam_rot_y = minmax_text_unwrap_or!(-360.0,360.0,form.cam_rot_y,0.0);
-
-    let mod_pos_x = minmax_text_unwrap_or!(-1000.0,1000.0,form.mod_pos_x,0.0);
-    let mod_pos_y = minmax_text_unwrap_or!(-1000.0,1000.0,form.mod_pos_y,0.25);
-    let mod_pos_z = minmax_text_unwrap_or!(-1000.0,1000.0,form.mod_pos_z,1.25);
-
-    let mod_rot_x = minmax_text_unwrap_or!(-360.0,360.0,form.mod_rot_x,0.0);
-    let mod_rot_y = minmax_text_unwrap_or!(-360.0,360.0,form.mod_rot_y,-15.0);
+    var_minmax_or_response!(mod_pos_x,form.mod_pos_x, -1000.0, 1000.0);
+    var_minmax_or_response!(mod_pos_y,form.mod_pos_y, -1000.0, 1000.0);
+    var_minmax_or_response!(mod_pos_z,form.mod_pos_z, -1000.0, 1000.0);
+    
+    // let mod_pos_x = minmax_text_unwrap_or!(-1000.0,1000.0,form.mod_pos_x,0.0);
+    // let mod_pos_y = minmax_text_unwrap_or!(-1000.0,1000.0,form.mod_pos_y,0.25);
+    // let mod_pos_z = minmax_text_unwrap_or!(-1000.0,1000.0,form.mod_pos_z,1.25);
+    var_minmax_or_response!(mod_rot_x,form.mod_rot_x, -360.0, 360.0);
+    var_minmax_or_response!(mod_rot_y,form.mod_rot_y, -360.0, 360.0);
+    // let mod_rot_x = minmax_text_unwrap_or!(-360.0,360.0,form.mod_rot_x,0.0);
+    // let mod_rot_y = minmax_text_unwrap_or!(-360.0,360.0,form.mod_rot_y,-15.0);
 
     let mesh = match skin_type {
         Skin::Alex => Arc::clone(&data.alex),
@@ -219,6 +273,8 @@ pub async fn render(MultipartForm(form): MultipartForm<UploadForm>,data: web::Da
         .build()
         .render();
     if resize {
+        var_minmax_or_response!(resize_width,form.resize_width, 10, 1000);
+        var_minmax_or_response!(resize_height,form.resize_height, 10, 1000);
         let resized = rendered
         .resize(resize_width,resize_height);
         let image = resized.result();
@@ -231,7 +287,7 @@ pub async fn render(MultipartForm(form): MultipartForm<UploadForm>,data: web::Da
                 ExtendedColorType::Rgba8,
             )
             .unwrap();
-        return HttpResponse::build(StatusCode::OK)
+        return HttpResponse::build(ActixStatusCode::OK)
                 .content_type("image/png")
                 .append_header(("Access-Control-Allow-Origin","*"))
                 .append_header(("Access-Control-Allow-Methods", "POST"))
@@ -247,7 +303,7 @@ pub async fn render(MultipartForm(form): MultipartForm<UploadForm>,data: web::Da
                 ExtendedColorType::Rgba8,
             )
             .unwrap();
-        return HttpResponse::build(StatusCode::OK)
+        return HttpResponse::build(ActixStatusCode::OK)
                 .content_type("image/png")
                 .append_header(("Access-Control-Allow-Origin","*"))
                 .append_header(("Access-Control-Allow-Methods", "POST"))
